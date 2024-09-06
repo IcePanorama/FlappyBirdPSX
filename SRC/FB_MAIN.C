705d64825bb6c10b578458e2fa6d59777c939b6f
IcePanorama <thriftedroot@protonmail.com>
2024-09-06 11:43:37 -0400

    Applied auto-formatting for better readability.

--- SRC/FB_MAIN.C
+++ SRC/FB_MAIN.C
@@ -9,27 +9,27 @@ int
 main (void)
 {
   Player_t player;
-  //POLY_F4 sprite;
+  // POLY_F4 sprite;
 
   init_video_subsystem (screen_buffer);
 
   SetPolyF4 (&player.sprite);
   SetShadeTex (&player.sprite, 1);
-  setRGB0(&player.sprite, 0, 255, 0);
+  setRGB0 (&player.sprite, 0, 255, 0);
 
   /*
    *  Currently not accounting for sprite width/height, but that'll be easily
    *  fixable once we switch to using a `SPRT_16` later.
    */
-  setXY4(&player.sprite, (SCREEN_WIDTH / 2) - 8, (SCREEN_HEIGHT / 2) + 8,
-                         (SCREEN_WIDTH / 2) + 8, (SCREEN_HEIGHT / 2) + 8,
-                         (SCREEN_WIDTH / 2) - 8, (SCREEN_HEIGHT / 2) - 8,
-                         (SCREEN_WIDTH / 2) + 8, (SCREEN_HEIGHT / 2) - 8);
+  setXY4 (&player.sprite, (SCREEN_WIDTH / 2) - 8, (SCREEN_HEIGHT / 2) + 8,
+          (SCREEN_WIDTH / 2) + 8, (SCREEN_HEIGHT / 2) + 8,
+          (SCREEN_WIDTH / 2) - 8, (SCREEN_HEIGHT / 2) - 8,
+          (SCREEN_WIDTH / 2) + 8, (SCREEN_HEIGHT / 2) - 8);
 
   while (1)
-  {
-    display_video (screen_buffer, 0);//&player.sprite);
-  }
+    {
+      display_video (screen_buffer, 0); //&player.sprite);
+    }
 
   return 0;
 }
--- SRC/V_VIDEO.C
+++ SRC/V_VIDEO.C
@@ -9,30 +9,30 @@
 void
 init_video_subsystem (ScreenBuffer_t *sb)
 {
-  //FIXME: need to determine NTSC/PAL and make changes accordingly
+  // FIXME: need to determine NTSC/PAL and make changes accordingly
   SetVideoMode (0);
-  GsInitGraph (SCREEN_WIDTH, SCREEN_HEIGHT, GsNONINTER|GsOFSGPU, 1, 0);
+  GsInitGraph (SCREEN_WIDTH, SCREEN_HEIGHT, GsNONINTER | GsOFSGPU, 1, 0);
 
-	FntLoad (960, 256);
-	SetDumpFnt (FntOpen (0, 8, SCREEN_WIDTH, 64, 0, 512));
-	SetGraphDebug (0);
+  FntLoad (960, 256);
+  SetDumpFnt (FntOpen (0, 8, SCREEN_WIDTH, 64, 0, 512));
+  SetGraphDebug (0);
 
-	InitGeom ();  // Init GTE
-	SetGeomOffset (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-	SetGeomScreen (DIST_TO_SCREEN);
+  InitGeom (); // Init GTE
+  SetGeomOffset (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+  SetGeomScreen (DIST_TO_SCREEN);
 
-	SetDefDrawEnv (&sb[0].draw_env, 0,   0, 320, 240);
-	SetDefDrawEnv (&sb[1].draw_env, 0, 240, 320, 240);
-	SetDefDispEnv (&sb[0].disp_env, 0, 240, 320, 240);
-	SetDefDispEnv (&sb[1].disp_env, 0,   0, 320, 240);
+  SetDefDrawEnv (&sb[0].draw_env, 0, 0, 320, 240);
+  SetDefDrawEnv (&sb[1].draw_env, 0, 240, 320, 240);
+  SetDefDispEnv (&sb[0].disp_env, 0, 240, 320, 240);
+  SetDefDispEnv (&sb[1].disp_env, 0, 0, 320, 240);
 
   sb[0].draw_env.isbg = sb[1].draw_env.isbg = 1;
-  setRGB0(&sb[0].draw_env, 0, 0, 0);
-  setRGB0(&sb[1].draw_env, 0, 0, 0);
+  setRGB0 (&sb[0].draw_env, 0, 0, 0);
+  setRGB0 (&sb[1].draw_env, 0, 0, 0);
 
-	//SetBackColor (0, 0, 0);  // Ambient color, set to black for the time being
+  // SetBackColor (0, 0, 0);  // Ambient color, set to black for the time being
 
-	SetDispMask (1);
+  SetDispMask (1);
 }
 
 void
@@ -43,7 +43,7 @@ display_video (ScreenBuffer_t *sb, POLY_F4 *sprite)
 
   ClearOTag (curr_sb->ordering_table, OT_MAX_LEN);
 
-  //setXY0(sprite, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+  // setXY0(sprite, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
   AddPrim (&curr_sb->ordering_table[1], sprite);
 
   DrawSync (0);
